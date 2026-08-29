7cb0bf32120da4363134f49413b7337da5acc38f
Holivane Holanda <holivane@gmail.com>
2026-08-29 18:17:37 -0300

    feat: criar layout base compartilhado (Header, navegação, Footer) (#50)
    Cria os componentes Header (logo + menu de navegação com react-router
    Link) e Footer, baseados no header/footer do site institucional. Cria
    o componente Layout, que envolve as páginas com Header + Outlet +
    Footer, e aplica esse layout em todas as rotas configuradas.
    
    Closes #13

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout.jsx'
 import Home from './pages/Home.jsx'
 import SobreOProjeto from './pages/SobreOProjeto.jsx'
 import Catalogo from './pages/Catalogo.jsx'
@@ -8,11 +9,13 @@ import Contato from './pages/Contato.jsx'
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/sobre-o-projeto" element={<SobreOProjeto />} />
-      <Route path="/catalogo" element={<Catalogo />} />
-      <Route path="/como-participar" element={<ComoParticipar />} />
-      <Route path="/contato" element={<Contato />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre-o-projeto" element={<SobreOProjeto />} />
+        <Route path="/catalogo" element={<Catalogo />} />
+        <Route path="/como-participar" element={<ComoParticipar />} />
+        <Route path="/contato" element={<Contato />} />
+      </Route>
     </Routes>
   )
 }
